75107b41f5dd22e6f53b7c0b44bb603c990d2dc0
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 15:15:41 +0530

    Add for #1326B : Maximums

---
+++ Codeforces/1326/B.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = list(map(int,input().split()))
+b = [-1000]*n
+
+b[0]=a[0];
+print(b[0],end = " ")
+
+m=b[0]
+
+for i in range(1,n):
+    b[i]=a[i]+m
+    m=max(m,b[i])
+    print(b[i],end = " ")
