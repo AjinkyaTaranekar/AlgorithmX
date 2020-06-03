7ef0f7e3517d1a5d43bde0c5650881048b725d12
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 21:53:33 +0530

    Add for #732A : Buy a Shovel

---
+++ Codeforces/732/A.py
@@ -0,0 +1,11 @@
+k,r = map(int,input().split())
+ans = 0
+sum = 0
+while True:
+    ans+=1
+    sum = ans*k
+    if sum % 10 == 0:
+        break
+    if (sum - r) % 10 == 0:
+        break
+print(ans)
