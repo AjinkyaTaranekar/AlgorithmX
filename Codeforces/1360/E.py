05a2e6361f454cb52e094a9ed19ec8f842a3e2b0
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 15:13:20 +0530

    Add for #1360E : Polygon

---
+++ Codeforces/1360/E.py
@@ -0,0 +1,17 @@
+testcase = int(input())
+for test in range(testcase):
+    n = int(input())
+    mat = []
+    for i in range(n):
+        row = list(input())
+        row = [ int(i) for i in row]
+        mat.append(row)
+    
+    flag = False
+    for i in range(n-1):
+        for j in range(n-1):
+            if mat[i][j]==1:
+                if mat[i+1][j]==0 and mat[i][j+1]==0:
+                    flag = True 
+                    break
+    print("YES" if not flag else "NO")
