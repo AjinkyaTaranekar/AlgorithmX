6392e69d7e5beedbe078f80c3f671be82f02c891
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 15:15:32 +0530

    Add for #1326C : Permutation Partitions

---
+++ Codeforces/1326/C.py
@@ -0,0 +1,18 @@
+mod=998244353
+n,k = list(map(int,input().split()))
+a = list(map(int,input().split()))
+maxi = []
+
+for i in range(n):
+    if(a[i]<=n and a[i]>n-k):
+        maxi.append(i)
+
+sum=0
+num=1
+for i in range(k):
+    sum+=(n-i)
+for i in range(1,k):
+	num*=(maxi[i]-maxi[i-1])
+	num%=mod
+
+print(sum,num)
