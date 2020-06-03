32d5c7e72f05d83023b3544feb66824398155338
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 16:14:22 +0530

    Add for #148A : Insomnia cure

---
+++ Codeforces/148/A.py
@@ -0,0 +1,6 @@
+k = int(input())
+l = int(input())
+m = int(input())
+n = int(input())
+d = int(input()) + 1
+print(len(set([i for i in range(k,d,k)]+[i for i in range(l,d,l)]+[i for i in range(m,d,m)]+[i for i in range(n,d,n)])))
