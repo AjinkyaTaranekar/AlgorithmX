a53caff9879f0851a0b8f6f4e7ade32a524bb862
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 15:54:09 +0530

    Add for #617A : Elephant

---
+++ Codeforces/617/A.py
@@ -0,0 +1,18 @@
+n = int(input())
+count=0
+while n-5 >= 0:
+    n-=5
+    count+=1
+while n-4 >= 0:
+    n-=4
+    count+=1
+while n-3 >= 0:
+    n-=3
+    count+=1
+while n-2 >= 0:
+    n-=2
+    count+=1
+while n-1 >= 0:
+    n-=1
+    count+=1
+print(count)
