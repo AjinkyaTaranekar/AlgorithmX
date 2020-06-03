929d64957b88ccb3a5a92996e8d309dbdc0a961d
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 15:15:23 +0530

    Add for #69A : Young Physicist

---
+++ Codeforces/69/A.py
@@ -0,0 +1,11 @@
+n = int(input())
+xsum,ysum,zsum=0,0,0
+for i in range(n):
+    x,y,z = map(int,input().split())
+    xsum+=x
+    ysum+=y
+    zsum+=z
+if xsum==0 and ysum==0 and zsum==0:
+    print("YES")
+else:
+    print("NO")
