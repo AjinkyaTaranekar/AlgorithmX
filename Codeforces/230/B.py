efa08fd87b25d86cda6128d0358b7039ab65a56b
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 18:06:25 +0530

    Add for #230B : T-primes

---
+++ Codeforces/230/B.py
@@ -0,0 +1,19 @@
+MAX=10**6
+prime=[True]*(MAX+1)
+prime[0]=prime[1]=False
+for i in range(2,int(MAX**0.5)):
+    if prime[i]==True:
+        for j in range(i*i,len(prime),i):
+            prime[j]=False
+ 
+n = int(input())
+nums = list(map(int,input().split()))
+for i in nums:
+    x=i**0.5
+    if x.is_integer():
+        if prime[int(x)]:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("NO")    
