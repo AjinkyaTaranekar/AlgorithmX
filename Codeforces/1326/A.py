f674b5a59186bc1c09c50ee11cc93e5dd0fae410
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 15:15:51 +0530

    Add for #1326A : Bad Ugly Numbers

---
+++ Codeforces/1326/A.py
@@ -0,0 +1,10 @@
+testCase = int(input())
+for i in range(testCase):
+    n = int(input())
+    if n == 1:
+        print(-1)
+    else:
+        print(2,end="")
+        for j in range(1,n):
+            print(3,end="")
+        print()
