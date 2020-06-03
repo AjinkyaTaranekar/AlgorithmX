d5ca4a6fa441b007c5be4a653d1710fddb72c38b
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 15:13:39 +0530

    Add for #1360B : Honest Coach

---
+++ Codeforces/1360/B.py
@@ -0,0 +1,9 @@
+testcase = int(input())
+for test in range(testcase):
+    n = int(input())
+    ath = list(map(int,input().split()))
+    ath.sort()
+    diff = 10**18
+    for i in range(n-1):
+        diff = min(diff , ath[i+1] - ath[i])
+    print(diff)
