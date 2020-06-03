5e11685318f5e3893f33e8e7dd3ae8a99b62d663
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 18:26:38 +0530

    Add for #492B : Vanya and Lanterns

---
+++ Codeforces/492/B.py
@@ -0,0 +1,7 @@
+n,l = map(int,input().split())
+nums = sorted(list(map(int,input().split())))
+maxi = 0
+for i in range(n - 1):
+    maxi = max(maxi, nums[i + 1] - nums[i])
+maxi = max(maxi / 2, nums[0], l - nums[n - 1])
+print("%.10f" % maxi)
