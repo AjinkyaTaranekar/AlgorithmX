01fcca4404e62ec0cef1a4bc781dcb98e4808189
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-03 22:58:57 +0530

    Add for #58A : Chat room

---
+++ Codeforces/58/A.py
@@ -0,0 +1,10 @@
+s = input()
+b = "hello"
+j = k = 0
+for i in range(len(s)):
+    if s[i] == b[j]:
+        j+=1
+        k+=1
+    if k == 5:
+        break;
+print("YES" if k == 5 else "NO")
