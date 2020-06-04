bb527320fc8b83462624c8a3b8f92b1c18992df8
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-04 20:14:59 +0530

    Add for #520A : Pangram

---
+++ Codeforces/520/A.py
@@ -0,0 +1,9 @@
+n = int(input())
+s = input().lower()
+if n < 26:
+    print("NO")
+else:
+    alpha = {chr(i+97):False for i in range(26)}
+    for i in s:
+        alpha[i] = True
+    print("YES" if sum(alpha.values()) == 26 else "NO" )
