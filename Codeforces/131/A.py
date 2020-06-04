9c3116e9a6f7b499832c6a5ea3dd72354fbf3391
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-06-04 19:54:02 +0530

    Add for #131A : cAPS lOCK

---
+++ Codeforces/131/A.py
@@ -0,0 +1,14 @@
+s = input()
+flag = True
+for i in range(1,len(s)):
+    if s[i].islower():
+        flag = False
+
+if flag:
+    for i in s:
+        if i.islower():
+            print(i.upper(),end='')
+        else:
+            print(i.lower(),end='')
+else:
+    print(s)
