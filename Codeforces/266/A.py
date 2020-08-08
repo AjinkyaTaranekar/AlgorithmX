ffd1f28acaa4dc6db0d9322bcb908038a5e352e7
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-08-08 12:55:32 +0530

    Add for #266A : Stones on the Table

---
+++ Codeforces/266/A.py
@@ -0,0 +1,11 @@
+n = int(input())
+s = [i for i in input()]
+res=0
+i = 1
+while(i<len(s)):
+    if s[i-1]==s[i]:
+        s.remove(s[i])
+        res+=1
+    else:
+        i+=1
+print(res)
