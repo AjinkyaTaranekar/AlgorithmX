7526ac9ba7ebeadaae0f168998bc9294d35197d1
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-08-10 11:53:32 +0530

    Add for #1391B : Fix You

---
+++ Codeforces/1391/B.py
@@ -0,0 +1,14 @@
+testcase = int(input())
+for test in range(testcase):
+    n,m = map(int,input().split())
+    grid = []
+    for i in range(n):
+        grid.append([i for i in input()])
+    count = 0
+    for i in range(m-1):
+        count += (grid[n-1][i] != 'R')
+      
+    for i in range(n-1):
+        count += (grid[i][m - 1] != 'D')
+    print(count)
+    
